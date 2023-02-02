73cc3f69169ac731c26aa8b099fe81f8882b9636
Vasily Yankovsky <yankvasya@gmail.com>
2023-02-03 00:01:13 +0400

    init: add webpack + customize `webpack.config`

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { randomFunction} from "./test";
+
+randomFunction('text!')
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+export const randomFunction = (text: string): void => {
+    console.log(`some random ${text} `)
+}
